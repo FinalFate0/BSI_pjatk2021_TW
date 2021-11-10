39aa737fd1b5970106bd420480033fd789db1e49
jay-bir <johnxpw@gmail.com>
2021-11-10 17:31:30 +0100

    fixed ex_0207

--- BSI_lab3/BSI_lab3/bsi_calculator_functions.cpp
+++ BSI_lab3/BSI_lab3/bsi_calculator_functions.cpp
@@ -1,4 +1,8 @@
+#include <cmath>
+
 double ex_0207_car_trip(double km_distance) {
 	const double failure_rate = 0.0001;
-	return 100 - km_distance * failure_rate;
+	return 1 - std::exp(failure_rate * km_distance);
+
+	
 }
