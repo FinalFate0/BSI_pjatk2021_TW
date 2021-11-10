92d0bbde4a1b3a9e8271be47c3d8d816413ced48
jay-bir <johnxpw@gmail.com>
2021-11-10 17:34:44 +0100

    fixed my fixed -goddamn minus- waiting for more

--- BSI_lab3/BSI_lab3/bsi_calculator_functions.cpp
+++ BSI_lab3/BSI_lab3/bsi_calculator_functions.cpp
@@ -2,7 +2,7 @@
 
 double ex_0207_car_trip(double km_distance) {
 	const double failure_rate = 0.0001;
-	return 1 - std::exp(failure_rate * km_distance);
+	return 1 - std::exp(-failure_rate * km_distance);
 
 	
 }
