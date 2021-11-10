4e312d5a95c5c8b05b228f96b40272bb9b5d584c
jay-bir <johnxpw@gmail.com>
2021-11-10 17:41:16 +0100

    done part a of 0206

--- BSI_lab3/BSI_lab3/bsi_calculator_functions.cpp
+++ BSI_lab3/BSI_lab3/bsi_calculator_functions.cpp
@@ -1,5 +1,11 @@
 #include <cmath>
 
+double ex_0206_a_device(double t_years) {
+	const double failure_rate = 0.7;
+	return 1 - std::exp(-failure_rate * km_distance);
+
+}
+
 double ex_0207_car_trip(double km_distance) {
 	const double failure_rate = 0.0001;
 	return 1 - std::exp(-failure_rate * km_distance);
