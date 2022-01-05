139036388cd37716eba29310ebe2e2620928c002
Tymoteusz Urbanowicz <dym7774@gmail.com>
2022-01-05 16:09:27 +0100

    added google login

--- OAuth2.0/app.py
+++ OAuth2.0/app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+    'client_secret.json',
+    scopes=['https://www.googleapis.com/auth/drive.metadata.readonly'])
 
-@app.route('/test')
+flow.redirect_uri = 'http://localhost:5000'
+
+authorization_url, state = flow.authorization_url(
+    access_type='offline',
+    include_granted_scopes='true')
+
+
+@app.route('/login')
+def hello():
+    return redirect(authorization_url)
+
+@app.route('/')
 def test():
     return render_template('index.html')
