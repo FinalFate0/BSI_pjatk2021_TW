b10e0c17282bb8ed45ecd897af498c50d4ce16b8
Tymoteusz Urbanowicz <dym7774@gmail.com>
2022-01-05 15:29:22 +0100

    oauth initial test

---
+++ OAuth2.0/oauth.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return render_template('index.html')
